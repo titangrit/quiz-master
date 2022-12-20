b28ae81ade1d6ff29dd41e1ab9d7c8a03fff520e
unknown <malemlink@gmail.com>
2022-12-21 00:06:28 +0530

    logging added

--- src/server/main.js
+++ src/server/main.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const path = require('path')
+const logger = require('./logger')
+const httpLogger = require('./httpLogger')
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(httpLogger)
 app.use(express.static(path.join(__dirname, "..", "..", "public")))
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
     res.status(200).sendFile(path.join(__dirname, "..", "..", "public", "index.html"))
 })
-  
-app.listen(PORT, () => console.log('Server running on port ' + PORT))
+
+app.listen(PORT, () => logger.info('Express.js listening on port ' + PORT))
